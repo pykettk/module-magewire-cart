e64f79fdba3afb4ba45482484470de09e1c9b2d8
pykettk <pykettk@gmail.com>
2022-06-07 18:58:13 +0100

    Create Clear Cart Component

---
+++ Magewire/Action/Clear.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright © element119. All rights reserved.
+ * See LICENCE.txt for licence details.
+ */
+declare(strict_types=1);
+
+namespace Element119\MagewireCart\Magewire\Action;
+
+use Element119\MagewireCart\Service\Cart as CartService;
+use Magewirephp\Magewire\Component;
+
+class Clear extends Component
+{
+    /** @var CartService */
+    private CartService $cartService;
+
+    /**
+     * @param CartService $cartService
+     */
+    public function __construct(
+        CartService $cartService
+    ) {
+        $this->cartService = $cartService;
+    }
+
+    /**
+     * @return void
+     */
+    public function execute(): void
+    {
+        if ($quote = $this->cartService->getQuote()) {
+            $this->cartService->saveQuote($quote->removeAllItems());
+        }
+    }
+}
